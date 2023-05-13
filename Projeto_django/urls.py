4681ea820414ac844d0df9551acbbd545c5b05bf
Fellipe Augusto <fellipe246@hotmail.com>
2023-05-12 21:41:23 -0300

    Projeto_django

--- Projeto_django/urls.py
+++ Projeto_django/urls.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+
+def my_view(request):
+    return HttpResponse('Uma pagina sobre view')
+
+
+def void(request):
+    return HttpResponse('Essa é a pagina inicial')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('view/', my_view),
+    path('', void),
+        
 ]
